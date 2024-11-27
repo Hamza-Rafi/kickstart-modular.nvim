0deac01d1d1075597fa3aac054c83453e298b6b4
Hamza Rafi <rafi.hamza2020@gmail.com>
2024-11-27 19:35:19 +0000

    Added keybind to open lazy.nvim

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -31,8 +31,8 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
--- Mason
 vim.keymap.set('n', '<leader>cm', '<cmd>Mason<cr>', { desc = 'Mason' })
+vim.keymap.set('n', '<leader>cl', '<cmd>Lazy<cr>', { desc = 'lazy.nvim' })
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
