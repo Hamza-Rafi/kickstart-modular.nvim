22ca0191c7baf01dea9054f2c6e640a36a803250
Hamza Rafi <rafi.hamza2020@gmail.com>
2024-11-27 19:32:50 +0000

    Added bufferline

---
+++ lua/custom/plugins/bufferline.lua
@@ -0,0 +1,22 @@
+return {
+  'akinsho/bufferline.nvim',
+  version = '*',
+  dependencies = 'nvim-tree/nvim-web-devicons',
+  config = function()
+    vim.opt.termguicolors = true
+    require('bufferline').setup {
+      options = {
+        diagnostics = 'nvim_lsp',
+        close_icon = '4',
+        offsets = {
+          {
+            filetype = 'neo-tree',
+            text = 'Neo-tree',
+            highlight = 'Directory',
+            text_align = 'left',
+          },
+        },
+      },
+    }
+  end,
+}
