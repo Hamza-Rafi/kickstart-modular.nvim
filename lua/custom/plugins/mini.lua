ea42bb1323fb62b691876a57ffca561f3a88025c
Hamza Rafi <rafi.hamza2020@gmail.com>
2025-08-31 03:20:10 +0100

    unnecesrry surround binds

--- lua/custom/plugins/mini.lua
+++ lua/custom/plugins/mini.lua
@@ -1,19 +1,3 @@
 return {
   { 'echasnovski/mini.comment', version = false },
-  {
-    'echasnovski/mini.surround',
-    version = false,
-    opts = {
-      -- https://www.lazyvim.or/extras/codin/mini-surround
-      mappings = {
-        add = 'gsa', -- Add surrounding in Normal and Visual modes
-        delete = 'gsd', -- Delete surrounding
-        find = 'gsf', -- Find surrounding (to the right)
-        find_left = 'gsF', -- Find surrounding (to the left)
-        highlight = 'gsh', -- Highlight surrounding
-        replace = 'gsr', -- Replace surrounding
-        update_n_lines = 'gsn', -- Update `n_lines`
-      },
-    },
-  },
 }
