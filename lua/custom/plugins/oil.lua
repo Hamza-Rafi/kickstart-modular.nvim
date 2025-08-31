577c49c363c921da09683ce2944db9521b7df194
Hamza Rafi <rafi.hamza2020@gmail.com>
2025-08-31 13:10:17 +0100

    added oil.nvim

---
+++ lua/custom/plugins/oil.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {
+      win_options = {
+        signcolumn = 'yes:2',
+      },
+    },
+    -- Optional dependencies
+    dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+    lazy = false,
+    keys = {
+      { '-', '<cmd>Oil<CR>', desc = 'Open parent dir in oil' },
+    },
+  },
+  {
+    'refractalize/oil-git-status.nvim',
+
+    dependencies = {
+      'stevearc/oil.nvim',
+    },
+
+    config = true,
+  },
+}
