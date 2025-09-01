43504ee8343dd23b726db8b988b83a99ae59ce7f
Hamza Rafi <rafi.hamza2020@gmail.com>
2025-09-01 14:40:40 +0100

    changed colourscheme to rose pine

--- lua/kickstart/plugins/colourscheme.lua
+++ lua/kickstart/plugins/colourscheme.lua
@@ -5,15 +5,43 @@ return {
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     'folke/tokyonight.nvim',
+    enabled = false,
     priority = 1000, -- Make sure to load this before all the other start plugins.
+    opts = {
+      style = 'storm',
+      transparent = true,
+      styles = {
+        sidebars = 'transparent',
+        floats = 'transparent',
+      },
+    },
+    -- config = function(_, opts)
+    --   require('tokyonight').setup(opts)
+    --   vim.cmd.colorscheme 'tokyonight'
+    -- end,
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'tokyonight'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
+    end,
+  },
+  {
+    'rose-pine/neovim',
+    name = 'rose-pine',
+    priority = 1000,
+    enabled = true,
+    opts = {
+      variant = 'moon',
+      styles = {
+        transparency = true,
+      },
+    },
+    init = function()
+      vim.cmd.colorscheme 'rose-pine'
     end,
   },
 }
