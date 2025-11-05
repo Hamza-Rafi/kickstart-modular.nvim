20c585d181637772496fb85e8ae1639ddde3eb71
Hamza Rafi <rafi.hamza2020@gmail.com>
2025-11-05 13:54:39 +0000

    added vimtex for LateX editing in nvim

---
+++ lua/custom/plugins/vimtex.lua
@@ -0,0 +1,20 @@
+return {
+  'lervag/vimtex',
+  lazy = false,
+  ft = { 'tex', 'plaintex', 'latex' },
+  init = function()
+    vim.g.vimtex_view_method = 'zathura'
+    --
+    -- Compiler (latexmk, continuous by default)
+    vim.g.vimtex_compiler_method = 'latexmk'
+    vim.g.vimtex_compiler_latexmk = {
+      options = { '-pdf', '-interaction=nonstopmode', '-synctex=1' },
+    }
+    vim.g.vimtex_compiler_continuous = 1
+
+    -- Optional quality-of-life
+    vim.g.vimtex_quickfix_mode = 0 -- don’t auto-open quickfix on warnings
+    vim.g.vimtex_mappings_enabled = 1 -- keep default mappings like \ll, \lv
+    vim.g.vimtex_syntax_enabled = 1
+  end,
+}
